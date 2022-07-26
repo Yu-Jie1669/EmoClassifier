a2795cebd203484de2c0d6ddc18046c96f627464
御姐 <162095214@qq.com>
2022-07-26 15:41:01 +0800

    modify loss

--- classifier/loss/label_smoothing.py
+++ classifier/loss/label_smoothing.py
@@ -10,18 +10,16 @@ def reduce_loss(loss, reduction='mean'):
     return loss.mean() if reduction == 'mean' else loss.sum() if reduction == 'sum' else loss
 
 
-
 class LabelSmoothingCrossEntropy(nn.Module):
-    def __init__(self,reduction, smoothing=0.0):
+    def __init__(self, reduction, smoothing=0.0):
         super(LabelSmoothingCrossEntropy, self).__init__()
         self.criterion = nn.CrossEntropyLoss()
         self.smoothing = smoothing
-        self.reduction=reduction
+        self.reduction = reduction
 
     def forward(self, logits, target):
         n = logits.size()[-1]
         log_preds = F.log_softmax(logits, dim=-1)
         loss = reduce_loss(-log_preds.sum(dim=-1), self.reduction)
         nll = F.nll_loss(log_preds, target, reduction=self.reduction)
-        return linear_combination(loss / n, nll, self.epsilon)
-
+        return linear_combination(loss / n, nll, self.smoothing)
