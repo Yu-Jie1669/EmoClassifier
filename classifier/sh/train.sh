a41e21af35550b35ea23783c3108bcb3d2086994
御姐 <162095214@qq.com>
2022-07-27 11:21:51 +0800

    modify sh

--- classifier/sh/train.sh
+++ classifier/sh/train.sh
@@ -1,2 +1,2 @@
 # baseline
-python ../bin/train.py --output=../../train/model-baseline.pkl --device=1 --batch_size=32 > ../../train/model-baseline.log
+python ../bin/train.py --output=../../train/model-baseline.pkl --device=1 --batch_size=40 > ../../train/model-baseline.log
